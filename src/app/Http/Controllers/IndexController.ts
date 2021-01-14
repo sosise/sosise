29f6fba2dd9fa698fbcc2898f924c4c4a728cc5d
Igor Kov <igor875126@gmail.com>
2021-01-14 13:04:03 +0600

    wip

--- src/app/Http/Controllers/IndexController.ts
+++ src/app/Http/Controllers/IndexController.ts
@@ -1,20 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
+import HttpResponse from 'sosise-core/build/Types/HttpResponse';
 
 export default class IndexController {
     /**
-     * Index
+     * Example method
      */
     public async index(request: Request, response: Response, next: NextFunction) {
         try {
-            // Prepare response
-            const responseType: ResponseType = {
+            // Prepare http response
+            const httpResponse: HttpResponse = {
                 code: 1000,
-                message: 'Example message',
+                message: 'Some example message',
                 data: null
             };
 
             // Send response
-            return response.send(responseType);
+            return response.send(httpResponse);
         } catch (error) {
             next(error);
         }
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+const test = require('./config/database').default;
+// TODO Debug (remove it)
+console.log(test);
+process.exit(0);
