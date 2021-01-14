e438a328b499a14548d30e446f31fc386a2e603d
dan <dangavrilin@gmail.com>
2021-01-14 16:20:34 +0600

    Manual build script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env sh
 
-# TODO
-echo "TODO"
+# Variables
+TAG=latest
+NAME=sosise
+IMAGENAME=${NAME}:${TAG}
+
+# Build app
+npm run build
+
+# Build docker image
+docker build -t ${IMAGENAME} -f docker/Dockerfile .
+
+# Run container
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo "To run the container use this command: ${GREEN}docker run --rm --name sosise -p 10000:10000 -v ${pwd}:/var/www/app/build ${IMAGENAME}${NC}"
