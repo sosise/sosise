7035375266db64b1d4b91920a9c848ad46cb33da
igor875126 <igor875126@gmail.com>
2024-08-12 19:16:59 +0500

    sosise-core updated + config/database.ts updated

--- src/config/database.ts
+++ src/config/database.ts
@@ -41,96 +41,96 @@ const databaseConfig = {
         /**
          * CockroachDB database connection
          */
-        cockroachdb: {
-            client: 'cockroachdb',
-            connection: {
-                host: process.env.DB_PROJECT_HOST || 'localhost',
-                port: Number(process.env.DB_PROJECT_PORT || 3306),
-                database: process.env.DB_PROJECT_DATABASE || 'sosise',
-                user: process.env.DB_PROJECT_USERNAME || 'root',
-                password: process.env.DB_PROJECT_PASSWORD || 'root',
-                // charset: 'utf8mb4',
-                // timezone: 'UTC',
-            },
-            /**
-             * Knex uses Tarn.js for pooling
-             * You can read more about it's configuration params here: https://github.com/vincit/tarn.js
-             */
-            pool: {
-                min: 2,
-                max: 10,
-                createTimeoutMillis: 60000,
-                acquireTimeoutMillis: 60000,
-                idleTimeoutMillis: 60000,
-                reapIntervalMillis: 2000,
-                createRetryIntervalMillis: 200,
-                propagateCreateError: false,
-            },
-            // debug: true,
-        },
+        // project: {
+        //     client: 'cockroachdb',
+        //     connection: {
+        //         host: process.env.DB_PROJECT_HOST || 'localhost',
+        //         port: Number(process.env.DB_PROJECT_PORT || 26257),
+        //         database: process.env.DB_PROJECT_DATABASE || 'defaultdb',
+        //         user: process.env.DB_PROJECT_USERNAME || 'root',
+        //         password: process.env.DB_PROJECT_PASSWORD || 'root',
+        //         // charset: 'utf8mb4',
+        //         // timezone: 'UTC',
+        //     },
+        //     /**
+        //      * Knex uses Tarn.js for pooling
+        //      * You can read more about it's configuration params here: https://github.com/vincit/tarn.js
+        //      */
+        //     pool: {
+        //         min: 2,
+        //         max: 10,
+        //         createTimeoutMillis: 60000,
+        //         acquireTimeoutMillis: 60000,
+        //         idleTimeoutMillis: 60000,
+        //         reapIntervalMillis: 2000,
+        //         createRetryIntervalMillis: 200,
+        //         propagateCreateError: false,
+        //     },
+        //     // debug: true,
+        // },
 
         /**
          * PostgreSQL database connection
          */
-        postgresql: {
-            client: 'pg',
-            connection: {
-                host: process.env.DB_PROJECT_HOST || 'localhost',
-                port: Number(process.env.DB_PROJECT_PORT || 3306),
-                database: process.env.DB_PROJECT_DATABASE || 'sosise',
-                user: process.env.DB_PROJECT_USERNAME || 'root',
-                password: process.env.DB_PROJECT_PASSWORD || 'root',
-                // charset: 'utf8mb4',
-                // timezone: 'UTC',
-            },
-            /**
-             * Knex uses Tarn.js for pooling
-             * You can read more about it's configuration params here: https://github.com/vincit/tarn.js
-             */
-            pool: {
-                min: 2,
-                max: 10,
-                createTimeoutMillis: 60000,
-                acquireTimeoutMillis: 60000,
-                idleTimeoutMillis: 60000,
-                reapIntervalMillis: 2000,
-                createRetryIntervalMillis: 200,
-                propagateCreateError: false,
-            },
-            // debug: true,
-        },
+        // project: {
+        //     client: 'pg',
+        //     connection: {
+        //         host: process.env.DB_PROJECT_HOST || 'localhost',
+        //         port: Number(process.env.DB_PROJECT_PORT || 26257),
+        //         database: process.env.DB_PROJECT_DATABASE || 'defaultdb',
+        //         user: process.env.DB_PROJECT_USERNAME || 'root',
+        //         password: process.env.DB_PROJECT_PASSWORD || 'root',
+        //         // charset: 'utf8mb4',
+        //         // timezone: 'UTC',
+        //     },
+        //     /**
+        //      * Knex uses Tarn.js for pooling
+        //      * You can read more about it's configuration params here: https://github.com/vincit/tarn.js
+        //      */
+        //     pool: {
+        //         min: 2,
+        //         max: 10,
+        //         createTimeoutMillis: 60000,
+        //         acquireTimeoutMillis: 60000,
+        //         idleTimeoutMillis: 60000,
+        //         reapIntervalMillis: 2000,
+        //         createRetryIntervalMillis: 200,
+        //         propagateCreateError: false,
+        //     },
+        //     // debug: true,
+        // },
 
         /**
          * MSSQL database connection
          */
-        mssql: {
-            client: 'mssql',
-            connection: {
-                server: 'someserver',
-                port: 1433,
-                database: 'database',
-                user: 'username',
-                password: 'password',
-                options: {
-                    enableArithAbort: true,
-                    encrypt: false
-                },
-                charset: 'utf8mb4',
-                timezone: 'UTC',
-            },
-            // requestTimeout: 30000,
-        },
+        // project: {
+        //     client: 'mssql',
+        //     connection: {
+        //         server: 'someserver',
+        //         port: 1433,
+        //         database: 'database',
+        //         user: 'username',
+        //         password: 'password',
+        //         options: {
+        //             enableArithAbort: true,
+        //             encrypt: false
+        //         },
+        //         charset: 'utf8mb4',
+        //         timezone: 'UTC',
+        //     },
+        //     // requestTimeout: 30000,
+        // },
 
         /**
          * SQLITE database connection
          */
-        sqlite: {
-            client: 'sqlite3',
-            connection: {
-                filename: process.env.SQLITE_FILE_PATH || process.cwd() + '/mydb.sqlite'
-            },
-            useNullAsDefault: true,
-        },
+        // sqlite: {
+        //     client: 'sqlite3',
+        //     connection: {
+        //         filename: process.env.SQLITE_FILE_PATH || process.cwd() + '/mydb.sqlite'
+        //     },
+        //     useNullAsDefault: true,
+        // },
     }
 };
 
